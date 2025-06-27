b6b331a0a50911aafddf5fb316714ac6b7303a42
vincenttommi <vincenttommi@gmail.com>
2025-06-27 20:49:07 +0300

    mapping our views to the urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,RegisterView
 
 
 
 urlpatterns  = [
-    path('', home, name='users-home')
+    path('', home, name='users-home'),
+    path('register/', RegisterView.as_view(), name='users-register'),
     
 ]
