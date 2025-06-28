3cc2e771c379db22fbd3416a25fee40bcfcbade8
vincenttommi <vincenttommi@gmail.com>
2025-06-28 13:35:19 +0300

    correcting  login form

--- users/forms.py
+++ users/forms.py
@@ -34,8 +34,8 @@ class RegisterForm(UserCreationForm):
 class  LoginForm(AuthenticationForm):
     username =  forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder':'Username','class':'form-control'}))      
     
-    password = forms.CharField(max_length=50,required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password':'class':'form-control','data-toggle':'password','id':'password':'name':'password'}))  
-    
+    password = forms.CharField(max_length=50,required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password','class':'form-control','data-toggle':'password','id':'password','name':'password'})) 
+     
     remember_me  = forms.BooleanField(required=False)
     
     
